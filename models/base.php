609d6e6f3970e5c3bbd5608d491a106abf19c03f
raquelroldo <raquelroldo@gmail.com>
2020-05-25 21:52:31 +0100

    Add validação e sanitização de dados.

--- models/base.php
+++ models/base.php
@@ -6,4 +6,21 @@ class Base {
     public function __construct() {
         $this->db = new PDO("mysql:host=localhost;dbname=wines_up;charset=utf8mb4", "root", "");
     }
+
+    public function sanitizer($input) {
+        foreach($input as $key => $value) {
+            $input[$key] = strip_tags(trim($value));
+        }
+
+        return $input;
+    }
+
+    public function isValidUser($data) {
+
+        if(isset($_SESSION["user_id"])) {
+            $user_id = $_SESSION["user_id"];
+        }
+        
+        return $user_id ?? false;
+    }
 }
