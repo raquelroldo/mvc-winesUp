ed2c4c7ec6d3643026e78eebd01e6836e48dedae
raquelroldo <raquelroldo@gmail.com>
2020-05-25 21:51:40 +0100

    Add footer e header ao controller.

--- controllers/wines.php
+++ controllers/wines.php
@@ -1,8 +1,10 @@
 <?php 
 require("models/wines.php");
     
-    $winesModel = new Wines();
+$winesModel = new Wines();
 
-    $wines = $winesModel->getList();
+$wines = $winesModel->getList();
 
+require("./assets/templates/header.php");
 require("views/wines.php");
+require("./assets/templates/footer.php");
